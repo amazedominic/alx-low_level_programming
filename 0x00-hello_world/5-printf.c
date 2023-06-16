31492473413090fee1bb52d7dda9ca29a596fec1
amazedominic <amazinggracedominic@yahoo.com>
2023-06-16 17:07:04 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-  *main - entry point
-  *return: always 0
-  */
+*main - entry point
+*return: always 0
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n"};
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
